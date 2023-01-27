b02b688fd5273adf54ced64de091669a08b2dd2e
Regina <gumerovars@gmail.com>
2023-01-27 16:47:05 +0500

    Решена задача

---
+++ task/Program.cs
@@ -0,0 +1,26 @@
+﻿void search(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {   
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void printArray (string[] array2)
+{
+    for (int i = 0; i < array2.Length; i++)
+    {
+        Console.Write($"{array2[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+string[] array1 = new string [4] {"Hello", "2", "world", ":-)"};
+string[] array2 = new string[array1.Length];
+search(array1,array2);
+printArray(array2);
